3834684efdb7ee2a168fbc8f02cc056cf22ff2f5
illichso <illichso@gmail.com>
2017-04-03 10:46:32 +0200

    format

--- src/main/java/illichso/TicTacToe.java
+++ src/main/java/illichso/TicTacToe.java
@@ -10,28 +10,25 @@ public class TicTacToe {
     }
 
     public String getResult() {
-        for(int i = 0; i < 3; i++){
-            if(board[i][0].equals(board[i][1]) &&  board[i][1].equals(board[i][2])){
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2])) {
                 return board[i][0];
             }
         }
 
-        for(int i = 0; i < 3; i++){
-            if(board[0][i].equals(board[1][i]) &&  board[1][i].equals(board[2][i])){
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i])) {
                 return board[0][i];
             }
         }
 
-        if(board[0][0].equals(board[1][1]) &&  board[1][1].equals(board[2][2])){
+        if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])) {
             return board[0][0];
         }
 
-        if(board[0][2].equals(board[1][1]) &&  board[1][1].equals(board[2][0])){
+        if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0])) {
             return board[0][2];
         }
-
-
-
 
         return D.getValue();
     }
